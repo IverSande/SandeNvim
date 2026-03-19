45f4d0d0946f396194a718a1aa078656518cbeb4
Iver Sande <iversande@gmail.com>
2026-03-19 12:52:30 +0100

    working setup very basic

---
+++ init.lua
@@ -0,0 +1,40 @@
+-- Enable line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Enable undo/redo changes even after closing and reopening a file
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+vim.o.confirm = true
+
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.lsp.config('*', {
+	root_markers = { '.git' },
+})
+
+vim.lsp.config('ts_ls', {
+	cmd = { 'typescript-language-server', '--stdio' },
+	filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+})
+
+vim.lsp.enable('ts_ls')
+
+vim.keymap.set("n", "gD", vim.lsp.buf.definition, { desc = "LSP: Go to definition" })
+vim.keymap.set("n", "grt", vim.lsp.buf.type_definition, { desc = "LSP: Type Definition" })
+vim.keymap.set("i", "<C-Space>", "<C-x><C-o>", { noremap = true })
+
+require("lua.config.lazy")
